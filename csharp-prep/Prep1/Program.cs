cffb38495eebf344521c58488cbf5363194467d3
Hannah Buckley <hannah@kebuckley.com>
2024-01-12 13:19:07 -0700

    completed prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
     }
 }
