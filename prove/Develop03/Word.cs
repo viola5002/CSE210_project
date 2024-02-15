7c7e8838e36a47e01c3a3688896521692f15da4d
Hannah Buckley <hannah@kebuckley.com>
2024-02-14 20:49:47 -0700

    made IsHidden() public for access in Scripture.cs file

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -2,12 +2,10 @@ using System;
 public class Word
 {
     private string _word;
-    private int _lengthOfWord;
 
     public Word(string randomWord)
     {
         _word = randomWord;
-        _lengthOfWord = _word.Length;
     }
 
     public bool IsHidden()
@@ -25,20 +23,13 @@ public class Word
     }
     public string HideWord()
     {
-        if (IsHidden())
+        foreach (char c in _word)
         {
-            return _word;
-        }
-        else
-        {
-            foreach (char c in _word)
+            if (c!=',' && c!=';' && c!='.' && c!=':' && c!='!' && c!='?')
             {
-                if (c!=',' && c!=';' && c!='.' && c!=':' && c!='!' && c!='?')
-                {
-                    _word = _word.Replace(c, '_');
-                }
+                _word = _word.Replace(c, '_');
             }
-            return _word;
         }
+        return _word;
     }
 }
