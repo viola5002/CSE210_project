576530d6c371bd1e3b6eade0a35135959506ef1e
Hannah Buckley <hannah@kebuckley.com>
2024-03-04 14:01:11 -0700

    complete prep activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+
+        Square sq = new Square("blue", 2);
+        //Console.WriteLine(sq.GetColor());
+        //Console.WriteLine(sq.GetArea());
+
+        Rectangle rect = new Rectangle("yellow", 3, 4);
+        //Console.WriteLine(rect.GetColor());
+        //Console.WriteLine(rect.GetArea());
+
+        Circle ccl = new Circle("red", 4);
+        //Console.WriteLine(ccl.GetColor());
+        //Console.WriteLine(ccl.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sq);
+        shapes.Add(rect);
+        shapes.Add(ccl);
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine();
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+
+        }
     }
 }
